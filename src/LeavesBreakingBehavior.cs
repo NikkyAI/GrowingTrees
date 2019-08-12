f2eed53ec40e0b4f4797806f9691f30aaeb97500
copygirl <copygirl@mcft.net>
2019-08-12 16:45:55 +0200

    Drops from leaves and skip for empty leaves

--- src/LeavesBreakingBehavior.cs
+++ src/LeavesBreakingBehavior.cs
@@ -1,3 +1,4 @@
+using System;
 using Vintagestory.API.Common;
 using Vintagestory.API.MathTools;
 
@@ -16,12 +17,35 @@ namespace GrowingTrees
 			IWorldAccessor world, BlockPos pos,
 			IPlayer byPlayer, ref EnumHandling handling)
 		{
+			// Execute default behavior if broken in creative mode.
 			if (byPlayer?.WorldData.CurrentGameMode == EnumGameMode.Creative) return;
-			handling = EnumHandling.PreventDefault;
 			
-			var code  = this.block.Code.ToString().Replace("leafybranch", "branch");
-			var block = world.GetBlock(new AssetLocation(code));
-			world.BlockAccessor.SetBlock(block.BlockId, pos);
+			// Replace branch block with non-leafy version.
+			var newCode  = block.Code.ToString().Replace("leafybranch", "branch");
+			var newBlock = world.GetBlock(new AssetLocation(newCode));
+			// If there is no associated non-leafy block, execute default behavior.
+			if (newBlock == null) return;
+			// Otherwise continue and replace with non-leafy version.
+			world.BlockAccessor.SetBlock(newBlock.BlockId, pos);
+			
+			// On server, drop items from the destroyed leaves. Like saplings!
+			if (world.Side == EnumAppSide.Server) {
+				var center = new Vec3d(pos.X + 0.5, pos.Y + 0.5, pos.Z + 0.5);
+				var drops  = block.GetDrops(world, pos, byPlayer, 1.0F) ?? new ItemStack[0];
+				foreach (var stack in drops) {
+					var maxStackSize = (block.SplitDropStacks ? 1 : stack.StackSize);
+					for (int i = 0; i < stack.StackSize; i += maxStackSize) {
+						var dropStack = stack.Clone();
+						dropStack.StackSize = Math.Min(maxStackSize, stack.StackSize - i);
+						world.SpawnItemEntity(dropStack, center, null);
+					}
+				}
+				world.PlaySoundAt(this.block.Sounds?.GetBreakSound(byPlayer),
+				                  center.X, center.Y, center.Z, byPlayer);
+			}
+			
+			// Override default behavior.
+			handling = EnumHandling.PreventDefault;
 		}
 	}
 }
